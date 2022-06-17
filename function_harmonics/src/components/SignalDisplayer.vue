794433ba389dd7cb909626dea1ebc748a26d5419
mikemrg17 <miguelangelmurga1234@gmail.com>
2022-06-16 21:22:40 -0500

    Python util added

--- function_harmonics/src/components/SignalDisplayer.vue
+++ function_harmonics/src/components/SignalDisplayer.vue
@@ -64,47 +64,9 @@ export default {
                 if ($vm.wave.length > 250) {
                     $vm.wave.pop();
                 }
-                /*let x = 0
-                let y = 0*/
-
-                /*for (let i = 0; i < this.slider.value(); i++) {
-                    let prevx = x;
-                    let prevy = y;
-                    
-                    let n = i * 2 + 1;
-                    let radius = 75 * (4 / (n * Math.PI));
-                    x += radius * Math.cos(n * this.time);
-                    y += radius * Math.sin(n * this.time);
-
-                    sketch.stroke(0, 100);
-                    sketch.noFill();
-                    sketch.ellipse(prevx, prevy, radius * 2);
-
-                    sketch.stroke(0);
-                    sketch.line(prevx, prevy, x, y);
-                }*/
-                /*
-                this.wave.unshift(y);
-                sketch.translate(200, 0);
-                sketch.line(x - 200, y, 0, this.wave[0]);
-                sketch.beginShape();
-                sketch.noFill();
-                for (let i = 0; i < this.wave.length; i++) {
-                    sketch.vertex(i, this.wave[i]);
-                }
-                sketch.endShape();
-
-                this.time += 0.05;
-
-                if (this.wave.length > 250) {
-                    this.wave.pop();
-                }*/
             }
         }
     },
-    /*render(h) {
-        return h(VueP5, {on: this});
-    }*/
 };
 </script>
 
